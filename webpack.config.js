0394f4dc1e34105020dfcfff84c635ad3ff16618
mrsmile <mr.smile_lp@mail.ru>
2019-03-18 23:14:01 +0300

    task 2

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js"
+    },
+    watch: NODE_ENV == 'development',
+    devtool: NODE_ENV == 'development' ? 'source-map' : null,
+
+    mode: NODE_ENV,
+
+    devServer: {
+        contentBase: './dist'
+    }
+}
