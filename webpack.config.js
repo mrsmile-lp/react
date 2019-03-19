13cd1039f145e703098c2f2c1455044fc135fdf2
Ilya Novikau <ilya_novikau@epam.com>
2019-03-19 17:10:54 +0300

    dev and prod mode weere added

--- webpack.config.js
+++ webpack.config.js
@@ -12,5 +12,16 @@ module.exports = {
 
     devServer: {
         contentBase: './dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
     }
 }
