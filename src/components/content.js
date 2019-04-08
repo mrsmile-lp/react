242cc9331138955cf5513976faa187f3780de15c
mrsmile <mr.smile_lp@mail.ru>
2019-04-09 00:06:05 +0300

    fetch function was created

--- src/components/content.js
+++ src/components/content.js
@@ -1,11 +1,32 @@
 import React from "react";
 
 class Content extends React.Component {
-    render() {
-        return(
-            <div className="content"></div>
-        )
+    constructor() {
+        super();
+        this.state = {
+            url: 'https://reactjs-cdp.herokuapp.com/movies',
+            content: {}
+        };
+
+       this.getMovies = this.getMovies.bind(this);
+    };
+    async getMovies(endpoint, id = null) {
+        const movies = await fetch(endpoint);
+        const jsonMovies = await movies.json();
+        this.setState({
+            content: jsonMovies
+        });
+        //console.log(this.state.content.data[0].id);
     }
+    render() {
+        this.getMovies(this.state.url);
+        console.log(state.content.data[0].id);
+        return(
+            <div className="content">
+                {this.state.content.data[0].id}
+            </div>
+        )
+    };
 }
 
 export default Content;
