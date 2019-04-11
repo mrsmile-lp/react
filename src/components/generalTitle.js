3352b0929e2af0cea6b7d09b45d574b8301ff5c0
mrsmile <mr.smile_lp@mail.ru>
2019-04-11 23:01:28 +0300

    refactoring statless components

--- src/components/generalTitle.js
+++ src/components/generalTitle.js
@@ -1,13 +1,11 @@
 import React from "react";
 
-class GeneralTitle extends React.Component {
-    render() {
-        return (
-            <div className="general-title">
-                <h1>netflixroulette</h1>
-            </div>
-        )
-    }
+function GeneralTitle() {
+  return (
+    <div className="general-title">
+      <h1>Netflixroulette</h1>
+    </div>
+  );
 }
 
 export default GeneralTitle;
